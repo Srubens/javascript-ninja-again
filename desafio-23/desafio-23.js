342a27f8224657c13ee91520af0b1182860df700
Rubens Filipe <rubensfilipe6@gmail.com>
2020-03-04 17:58:45 -0300

    add branch desafio-23

--- desafio-23/desafio-23.js
+++ desafio-23/desafio-23.js
@@ -1,23 +1,121 @@
-/*
-Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
-As regras são:
-- Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
-diretamente;
-- O input deve iniciar com valor zero;
-- Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
-- Deve haver 4 botões para as operações principais: soma (+), subtração(-),
-multiplicação(x) e divisão(÷);
-- Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
-que irá limpar o input, deixando-o com valor 0;
-- A cada número pressionado, o input deve atualizar concatenando cada valor
-digitado, como em uma calculadora real;
-- Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
-operação no input. Se o último caractere no input já for um símbolo de alguma
-operação, esse caractere deve ser substituído pelo último pressionado.
-Exemplo:
-- Se o input tem os valores: "1+2+", e for pressionado o botão de
-multiplicação (x), então no input deve aparecer "1+2x".
-- Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
-input;
-- Ao pressionar o botão "CE", o input deve ficar zerado.
-*/
+(function(window, document){
+	
+	'use script';
+
+	/*
+	Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
+	As regras são:
+	- Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
+	diretamente;
+	- O input deve iniciar com valor zero;
+	- Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
+	- Deve haver 4 botões para as operações principais: soma (+), subtração(-),
+	multiplicação(x) e divisão(÷);
+	- Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
+	que irá limpar o input, deixando-o com valor 0;
+	- A cada número pressionado, o input deve atualizar concatenando cada valor
+	digitado, como em uma calculadora real;
+	- Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
+	operação no input. Se o último caractere no input já for um símbolo de alguma
+	operação, esse caractere deve ser substituído pelo último pressionado.
+	Exemplo:
+	- Se o input tem os valores: "1+2+", e for pressionado o botão de
+	multiplicação (x), então no input deve aparecer "1+2x".
+	- Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
+	input;
+	- Ao pressionar o botão "CE", o input deve ficar zerado.
+	*/
+
+	let $visor = document.querySelector('[data-js="visor"]')
+	let $btnNumber = document.querySelectorAll('[data-js="btn-number"]')
+	let $btnC = document.querySelector('[data-js="btn-c"]')
+	let $btnOperation = document.querySelectorAll('[data-js="btn-operation"]')
+	let $btnEqual = document.querySelector('[data-js="btn-equal"]')
+	let $btnBackSpace = document.querySelector('[data-js="btn-backspace"]')
+
+	// console.log($visor,$btnNumber,$btnC,$btnOperation,$btnEqual)
+
+	function btnArray(btnEvent, fncExec){
+		return Array.prototype.forEach.call(btnEvent, function(btn){
+			btn.addEventListener('click', fncExec, false)
+		})
+	}
+
+	// $btnC.addEventListener('click', handleClickC, false)
+	// $btnEqual.addEventListener('click', handlerClicklEqual, false)
+
+	function events(event, fncExec){
+		return event.addEventListener('click', fncExec, false)
+	}
+
+	function handleClickC(){
+		$visor.value = 0
+	}
+
+	function handleClickNumber(){
+		$visor.value += this.value
+	}
+
+	function handleClickOperation(){
+
+		$visor.value = removeLastItemIfitAnOperator($visor.value)
+		$visor.value += this.value
+	}
+
+	function removeLastItemIfitAnOperator(number){
+
+		if( isLastItemAnOperation(number) ){
+			return number.slice(0, -1)
+		}
+		return number;
+	}
+
+	function isLastItemAnOperation(number){
+		
+		let operations = ['+', '-', 'x', '÷']
+		let lastItem = number.split('').pop()
+
+		return operations.some(function (operator){
+			return operator === lastItem
+		})
+
+	}
+
+	function handlerClicklEqual(){
+	    
+	    $visor.value = removeLastItemIfitAnOperator($visor.value);
+	    let allValues = $visor.value.match(/\d+[+x÷-]?/g);
+
+	    let result = allValues.reduce(function(accumulated,actual){
+	      
+	      let firstValue = accumulated.slice(0, -1);
+	      let operator = accumulated.split('').pop();
+	      let lastValue = removeLastItemIfitAnOperator(actual);
+	      let lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
+
+	      switch(operator){
+	        case '+':
+	          return (Number(firstValue) + Number(lastValue)) + lastOperator;
+	        case '-':
+	          return (Number(firstValue) - Number(lastValue)) + lastOperator;
+	        case 'x':
+	          return (Number(firstValue) * Number(lastValue)) + lastOperator;
+	        case '÷':
+	          return (Number(firstValue) / Number(lastValue)) + lastOperator;
+	      }
+
+	        return accumulated + actual;
+	    });
+
+	    console.log(result);
+	    $visor.value = result;
+    }
+
+    btnArray($btnNumber, handleClickNumber)
+	btnArray($btnOperation, handleClickOperation)
+
+	events($btnC,handleClickC)
+	events($btnEqual,handlerClicklEqual)
+
+
+})(window, document);
