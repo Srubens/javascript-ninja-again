32576b5e34ff65d8aa1d856c9eb6f0348dd3ac00
Rubens Filipe <rubensfilipe6@gmail.com>
2020-03-24 18:05:50 -0300

    add branch 25-01

--- desafio-01/desafio-01.js
+++ desafio-01/desafio-01.js
@@ -1,40 +1,56 @@
-// Desafio Semana #1
+(function(){
+    
+    "use script";
 
-// Declarar uma variável chamada `myvar`, sem valor.
-// ?
+    // Desafio Semana #1
 
-// Após declarada, atribua o valor 10 à variável `myvar`.
-// ?
+    // Declarar uma variável chamada `myvar`, sem valor.
+    let myvar;
+    console.log(myvar)
 
-// Declare uma nova variável chamada `soma`, e adicione uma instrução somando os valores 15 e 8.
-// ?
+    // Após declarada, atribua o valor 10 à variável `myvar`.
+    myvar = 10
+    console.log(myvar)
 
-// Atribua à variável `soma` todo o valor dela, somando 1, usando o operador de soma abreviado.
-// ?
+    // Declare uma nova variável chamada `soma`, e adicione uma instrução somando os valores 15 e 8.
+    let soma = 15 + 8;
+    console.log(soma)
 
-// Atribua à variável `soma` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
-// ?
+    // Atribua à variável `soma` todo o valor dela, somando 1, usando o operador de soma abreviado.
+    soma++
+    console.log(soma)
 
-// Qual é o valor da variável `soma` até aqui?
-// ?
+    // Atribua à variável `soma` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
+    soma *= 3
+    console.log(soma)
 
-// Declare uma variável chamada `souninja`, atribuindo à ela o valor booleano que representa `verdadeiro`.
-// ?
+    // Qual é o valor da variável `soma` até aqui?
+    console.log(soma)
 
-// Declare uma variável chamada `comida` que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
-// ?
+    // Declare uma variável chamada `souninja`, atribuindo à ela o valor booleano que representa `verdadeiro`.
+    let souninja = true 
+    console.log(souninja)
 
-// Digite a instrução que imprime o valor de 'feijao', que está na variável `comida`.
-// ?
+    // Declare uma variável chamada `comida` que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
+    let comida = ['arroz', 'feijão', 'ovo']
+    console.log(comida)
 
-// Digite o código que verifica se a variável `soma' é igual a variável `myvar` (testando também o tipo).
-// ?
+    // Digite a instrução que imprime o valor de 'feijao', que está na variável `comida`.
+    console.log(comida[1])
 
-// Digite o código que verifica se a variável `myvar` é menor ou igual à variável `soma`.
-// ?
+    // Digite o código que verifica se a variável `soma' é igual a variável `myvar` (testando também o tipo).
+    console.log(soma == myvar)
+    console.log(soma === myvar)
 
-// Crie uma função chamada `divisao` que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
-// ?
+    // Digite o código que verifica se a variável `myvar` é menor ou igual à variável `soma`.
+    console.log(myvar <= soma)
 
-// Invoque a função criada acima, passando os parâmetros 10 e 2.
-// ?
+    // Crie uma função chamada `divisao` que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
+    function divisao(a,b){
+        return a / b
+    }
+
+    // Invoque a função criada acima, passando os parâmetros 10 e 2.
+    console.log(divisao(10, 5))
+
+})()
